79e77511c84b8c9a7f1056385e007d90ba21a1dd
Brandon Marlow <brandon.n.marlow@gmail.com>
2019-10-14 20:56:57 -0400

    initial commit

---
+++ Dockerfile
@@ -0,0 +1,56 @@
+FROM centos:centos7
+
+#add my files
+ADD reqs.txt /root
+RUN mkdir /root/tensor
+ADD tensor/play.py /root/tensor/
+ADD tensor/record.py /root/tensor/
+ADD tensor/train.py /root/tensor/
+ADD tensor/utils.py /root/tensor/
+RUN mkdir /root/uploads/
+RUN chmod 777 /root/uploads
+RUN mkdir /root/flask
+ADD flask/app.py root/flask/
+ADD flask/main.py root/flask/
+RUN chmod -R 777 /root/tensor
+
+RUN mkdir /root/downloads
+RUN chmod 777 /root/downloads
+RUN mkdir /root/results
+RUN chmod 777 /root/results
+RUN mkdir /root/processing
+RUN chmod 777 /root/processing
+RUN mkdir /root/processed
+RUN chmod 777 /root/processed
+
+
+#install pre-reqs
+RUN yum -y install freetype-devel libpng gcc gcc-c++ wget epel-release dkms grub2 make centos-release-scl python3 python3-devel python3-tkinter shutil
+RUN yum -y groupinstall 'Development Tools'
+
+
+#install python pre-reqs
+RUN pip3 install --upgrade pip
+RUN pip3 install numpy flask kafka app requests
+RUN pip3 install -r /root/reqs.txt
+
+
+
+
+#install cuda 10.0 libs
+RUN wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda_10.0.130_410.48_linux -O /root/cuda_10.0.130_410.48_linux --quiet
+RUN sh /root/cuda_10.0.130_410.48_linux --silent --toolkit --toolkitpath=/usr/local/cuda-10.0
+
+#Get cudnn libs from NVIDIA
+RUN wget https://www.dropbox.com/s/l5t159ily3m10sg/cudnn-10.0-linux-x64-v7.6.4.38.tgz?dl=0 -O /root/cudnn-10.0-linux-x64-v7.6.4.38.tgz --quiet
+RUN tar -C /usr/local/ -xzf /root/cudnn-10.0-linux-x64-v7.6.4.38.tgz
+
+
+
+RUN mkdir /root/data
+RUN wget https://www.dropbox.com/s/x0orqhrfihf6hsz/x.npy?dl=0 -O /root/tensor/data/X.npy --quiet
+RUN wget https://www.dropbox.com/s/w7ckfpjac9ckkuw/y.npy?dl=0 -O /root/tensor/data/y.npy --quiet
+
+
+
+CMD python3 - u /root/flask/main.py
---
+++ flask/main.py
@@ -0,0 +1,86 @@
+import os
+import urllib.request, requests
+import shutil, kafka, logging, time, datetime
+
+def main_loop():
+    files = []
+    print('Streams initiated...')
+    while True:
+        consumer_received = kafka.KafkaConsumer('file-received', bootstrap_servers='my-cluster-kafka-bootstrap:9092', consumer_timeout_ms=10000)
+        for message in consumer_received:
+            str_message = bytes.decode(message.value)
+            print(str_message)
+            #if its stupid but it works...  well this is still stupid
+            filename = str(str_message.split(': ')[1:])
+            filename = filename.replace("'", '')
+            filename = filename.replace('[', '')
+            filename = filename.replace(']', '')
+            files.append(filename)
+
+            if len(files) == 2:
+                get_files(files)
+                files = []
+
+
+def get_files(files):
+    for file in files:
+        print('retrieving file from dropoff pod ' + file)
+        baseurl = "http://dropoff-marlowkart.apps.lakitu.hosted.labgear.io/files/"
+        url = baseurl + file
+        dlpath = '/root/downloads/'
+        dlpathwithfile = dlpath + file
+        r = requests.get(url)
+        open(dlpathwithfile, 'wb').write(r.content)
+
+    process_training_files(files)
+    pass
+
+
+def process_training_files(files):
+    dlpath = '/root/downloads/'
+    processingpath = '/root/data/'
+    for file in files:
+        shutil.move(dlpath + file, processingpath + file)
+
+
+    #for timestamping results file
+    now = datetime.datetime.now()
+    dt_string = now.strftime("%d%m%Y-%H%M%S")
+
+    #issue ML commands
+    #####JUST A STUBOUT
+    subprocess.run('/root/tensor/train.py')
+
+
+    ###GRAB screen output?
+
+    ###GRAB file output and send back
+
+
+
+
+    temp_file = open("/root/results/results--" + dt_string + '.txt', "w")
+    temp_file.write("This is an empty results file")
+    temp_file.close()
+    results_file = 'results--' + dt_string + '.txt'
+    ###end stubout
+
+    send_file(results_file)
+    pass
+
+
+def send_file(file):
+    uploadapiurl = 'http://dropoff-marlowkart.apps.lakitu.hosted.labgear.io/api-upload'
+    resultsbasepath = '/root/results/'
+    resultsfullpath = resultsbasepath + str(file)
+    myfile = {'file': open(resultsfullpath, 'rb')}
+    response = requests.post(uploadapiurl, files=myfile)
+    if response.status_code == 201:
+        print('results file ' + file + ' successfully sent to dropoff pod')
+    else:
+        print('something went wrong, try again')
+    pass
+
+if __name__ == "__main__":
+    main_loop()
+
