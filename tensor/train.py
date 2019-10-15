5abd8540e8c1927b9f2b1c7fe0c0669c47102488
Brandon Marlow <brandon.n.marlow@gmail.com>
2019-10-15 02:04:56 -0400

    not sure how that snuck in there

--- tensor/train.py
+++ tensor/train.py
@@ -6,7 +6,7 @@ from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D
 from keras import optimizers
 from keras import backend as K
-from tensor.utils import Sample
+from utils import Sample
 
 # Global variable
 OUT_SHAPE = 5
